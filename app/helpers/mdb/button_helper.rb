4dcb28f17943627122afe6e5009caa03f502e188
KhoaRB <ThanhKhoaIT@gmail.com>
2018-11-08 04:38:19 +0700

    [Buttons] Restyle

--- app/helpers/mdb/button_helper.rb
+++ app/helpers/mdb/button_helper.rb
@@ -3,17 +3,15 @@ module Mdb
 
     def mdb_button(text, options = {})
       styles = mdb_css_class_names(options[:class], 'btn')
-      styles << " waves-effect" if options[:effect]
+      styles << " waves-effect" if options[:dark]
       styles << " waves-light" if options[:light]
       styles << " btn-rounded" if options[:rounded]
       styles << " btn-floating" if options[:floating]
-      styles << " peach-gradient" if options[:g_peach]
-      styles << " purple-gradient" if options[:g_purple]
-      styles << " blue-gradient" if options[:g_blue]
-      styles << " aqua-gradient" if options[:g_aqua]
+      styles << " #{options[:gradient]}-gradient" if options[:gradient]
       styles << " btn-#{options[:size]}" if options[:size]
       styles << " btn-#{options[:style]}" if options[:style] && !options[:outline]
       styles << " btn-outline-#{options[:style]}" if options[:style] && options[:outline]
+      styles << " btn-mdb-color" unless options[:style]
       concat content_tag :button, text, type: :button, class: styles
       content_tag(:span, options[:counter], class: :counter) if options[:counter]
     end
